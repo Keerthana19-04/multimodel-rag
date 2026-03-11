e3efd708a68c9c4b1dbe4875d047acec1b0af2d9
Keerthana <petlakeerthana@email.com>
2026-03-11 23:28:29 +0530

    Day 4: Chromadb persistent storage + metadata filtering + CRUD operations

--- src/test_gemini.py
+++ src/test_gemini.py
@@ -17,3 +17,25 @@ print("=" * 50)
 print("GROQ RESPONSE:")
 print("=" * 50)
 print(response.choices[0].message.content)
+
+
+
+# from sentence_transformers import SentenceTransformer
+# from sklearn.metrics.pairwise import cosine_similarity
+
+# model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# s1 = "I love eating pizza"
+# s2 = "Pizza is my favourite food"
+# s3 = "I love coding Python"
+
+# emb = model.encode([s1, s2, s3])
+
+# print("S1 vs S2:", cosine_similarity([emb[0]], [emb[1]])[0][0])
+# print("S1 vs S3:", cosine_similarity([emb[0]], [emb[2]])[0][0])
+# print("S2 vs S3:", cosine_similarity([emb[1]], [emb[2]])[0][0])
+
+
+
+
+
